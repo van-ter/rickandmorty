b87bf86cf1ff7097baa7fa4c8af21070f3147cfc
van-ter <ivantr1995@gmail.com>
2022-11-01 11:56:24 +0200

    settings

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    assetsDir: 'rickandmorty'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
