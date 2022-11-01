aa668e329859f64181d5d28c94117c6a46576b42
van-ter <ivantr1995@gmail.com>
2022-11-01 11:19:03 +0200

    settings

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
+      <p>started page</p>
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
